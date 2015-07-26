15a736710ec798cf5bdca73c96ae2c0d1eb7f539
chongdashu <me@chongdashu.com>
2015-07-26 19:52:49 -0400

    pants noralized.

--- scripts/image_processing.py
+++ scripts/image_processing.py
@@ -12,24 +12,17 @@ def is_shirt(imageName):
     return imageName.find("_short") >= 0 or imageName.find("_shorter") >= 0 or imageName.find("_long") >= 0
 
 
-images = []
-prefixes = []
+def normalize_shirts_or_pants(shirtsOrPants="Shirts"):
 
-
-def main():
-    '''
-    Shirts - Arms
-    '''
-    global images
-    global prefixes
-
-    shirtsFolder = core.PNG_PATH + "Shirts"
+    shirtsFolder = core.PNG_PATH + shirtsOrPants
     images = [image for image in core.get_files_in_folder(shirtsFolder) if is_shirt(image)]
 
     prefixes = list(set([image.split("_")[0] for image in images]))
 
     for prefix in prefixes:
+
         print "Normalizing %s" % (prefix)
+
         longImage = Image.open(core.get_image_path(prefix + "_long.png", core.PNG_PATH))
         shortImage = Image.open(core.get_image_path(prefix + "_short.png", core.PNG_PATH))
         shorterImage = Image.open(core.get_image_path(prefix + "_shorter.png", core.PNG_PATH))
@@ -41,6 +34,21 @@ def main():
         newShorterImage = Image.new("RGBA", longImage.size)
         newShorterImage.paste(shorterImage, (0, 0))
         newShorterImage.save(core.get_image_path(prefix + "_shorter.png", core.PNG_PATH))
+
+
+images = []
+prefixes = []
+
+
+def main():
+    '''
+    Shirts - Arms
+    '''
+    global images
+    global prefixes
+
+    # normalize_shirts_or_pants("Shirts")
+    normalize_shirts_or_pants("Pants")
 
 if __name__ == "__main__":
     main()
